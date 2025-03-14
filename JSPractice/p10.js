20bc355c4232e9b7b45855a3980113f9c0baf96a
Carter Leitch <csleitch@students.unwsp.edu>
2025-03-14 17:59:42 -0500

    p10

---
+++ JSPractice/p10.js
@@ -0,0 +1,11 @@
+function doThing(userInput){
+    let result = 1;
+    for (number of userInput){
+        result *= number;
+    }
+
+    return result;
+}
+
+let input = [2, 3, 4];
+console.log(doThing(input));
